366399c4c65c1739bf4804c8b68c53b28c2e8a4a
Kássio <sapokk.pinga@gmail.com>
2024-08-20 19:30:18 -0300

    so testando

--- Testes/teste-2.py
+++ Testes/teste-2.py
@@ -4,6 +4,13 @@ def calcular_media(numeros):
     media = soma/quantidade
     return media
 
-notas=[8,7,9,10]
+notas = []
+
+notas.append(float(input("Digite a nota 1 do aluno: ")))
+notas.append(float(input("Digite a nota 2 do aluno: ")))
+notas.append(float(input("Digite a nota 3 do aluno: ")))
+notas.append(float(input("Digite a nota 4 do aluno: ")))
+
 media_final=calcular_media(notas)
+
 print(f"A media é {media_final}")
